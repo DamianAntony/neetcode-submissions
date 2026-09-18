e22e6809cd42d7fe15a1abd4b311e5804272b876
Damian Antony <146168217+DamianAntony@users.noreply.github.com>
2026-09-18 22:01:49 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+      
+        int n = nums.size();
+
+        unordered_map<int, int> mp;
+
+        for(int num:nums){
+            mp[num]++;
+
+            if(mp[num]>1){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+};
